180481a88609b17dc874899e5e00d69cb8b1ac5f
Seantheman01 <sean.v.g@outlook.com>
2025-11-05 13:59:23 +0100

    .

--- Javascript/Persoonsregister/register.js
+++
@@ -1,7 +0,0 @@
-fetch('opdracht1_bijlage.json')
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-    document.getElementById("demo").textContent = JSON.stringify(data, null, 2);
-  })
-  .catch(err => console.error("Error loading JSON:", err));
