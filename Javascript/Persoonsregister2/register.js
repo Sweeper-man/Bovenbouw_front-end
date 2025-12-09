552ef42c2be63005ad8aef09884ddf24bb538999
Seantheman01 <sean.v.g@outlook.com>
2025-12-09 10:26:04 +0100

    working register.js

--- Javascript/Persoonsregister2/register.js
+++ Javascript/Persoonsregister2/register.js
@@ -1,6 +1,18 @@
 fetch("opdracht2_bijlage.json")
-.then(response => response.json())
-.then(data => {
-    document.getElementById("output").textContent =
-    JSON.stringify(data, null, 2);
+  .then(response => response.json())
+  .then(data => {
+
+    let len = data.length;
+    let text = "";
+
+    for (let i = 0; i < len; i++) {
+      text += 
+        data[i].voornaam + " " +
+        data[i].achternaam + ", " +
+        data[i].nationaliteit + ", " +
+        data[i].leeftijd + " jaar, " +
+        data[i].gewicht + " kg. <br>";
+    }
+
+    document.getElementById("output").innerHTML = text;
 });
